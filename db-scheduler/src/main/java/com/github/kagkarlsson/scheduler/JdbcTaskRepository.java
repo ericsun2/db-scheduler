b6497b9b2d25e8e57ae1654daec7b97c53ab65e8
Zhuo (Joe) Wang <zhuowang@linkedin.com>
2020-04-20 10:47:30 -0700

    remove comments & remove build.gradle

--- db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/JdbcTaskRepository.java
+++ db-scheduler/src/main/java/com/github/kagkarlsson/scheduler/JdbcTaskRepository.java
@@ -220,19 +220,7 @@ public class JdbcTaskRepository implements TaskRepository {
 		}
 		return updated > 0;
 	}
-/*
-task_name varchar(40) not null,
-    task_instance varchar(40) not null,
-    task_data blob,
-    execution_time timestamp(6) not null,
-    picked BOOLEAN not null,
-    picked_by varchar(50),
-    last_success timestamp(6) null,
-    last_failure timestamp(6) null,
-    consecutive_failures INT,
-    last_heartbeat timestamp(6) null,
-    version BIGINT not null,
- */
+
 	private void insertHistory(Execution execution) {
 	    try {
             jdbcRunner.execute("insert into " + persistHistoryTableName
